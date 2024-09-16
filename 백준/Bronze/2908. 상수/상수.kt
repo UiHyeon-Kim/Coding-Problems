d78af2cd16d4749df97ce2cb77abe1bee1f622df
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2024-09-16 12:52:46 +0900

    [Bronze II] Title: 상수, Time: 96 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.kt
+++ 백준/Bronze/2908. 상수/상수.kt
@@ -1,9 +1,9 @@
+import kotlin.math.max
+
 fun main() {
     val (A, B) = readln().split(" ")
 
-    val num1 = A.reversed()
-    val num2 = B.reversed()
-
-    if (num1 > num2) println(num1)
-    else println(num2)
+    val num1 = A.reversed().toInt()
+    val num2 = B.reversed().toInt()
+    println(max(num1,num2))
 }
