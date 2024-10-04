752c64eb354c19e1e1a5b50c1a59492eb917d25c
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2024-10-04 16:50:55 +0900

    [level 2] Title: 튜플, Time: 377.72 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.kt
+++ 프로그래머스/2/64065. 튜플/튜플.kt
@@ -1,13 +1,12 @@
 class Solution {
     fun solution(s: String): List<Int> {
         var strs = s.split(",{")
-        var tmp = mutableListOf<List<String>>()
+        var tmp = mutableSetOf<List<String>>()
 
         repeat(strs.size){
             tmp.add(strs[it].filter { it !in setOf('{', '}') }.split(","))
         }
-        tmp.sortBy { it.size }
 
-        return tmp.flatten().distinct().map { it.toInt() }
+        return tmp.sortedBy { it.size }.flatten().distinct().map { it.toInt() }
     }
 }
