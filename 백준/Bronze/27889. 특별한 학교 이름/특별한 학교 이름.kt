b4c9222c292b913eb7396c2a31f97b796912a639
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2024-10-31 14:11:35 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 80 ms, Memory: 12236 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.kt
@@ -0,0 +1,8 @@
+fun main() {
+    when (readln()) {
+        "NLCS" -> println("North London Collegiate School")
+        "BHA" -> println("Branksome Hall Asia")
+        "KIS" -> println("Korea International School")
+        "SJA" -> println("St. Johnsbury Academy")
+    }
+}
