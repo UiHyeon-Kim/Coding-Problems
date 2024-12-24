63489ecbdfa4f105735e1303f548e00bc86d7df6
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2024-12-25 02:02:36 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 20944 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.kt
+++ 백준/Silver/2606. 바이러스/바이러스.kt
@@ -1,36 +1,27 @@
-import java.util.*
+fun dfs(node: Int, network: List<MutableList<Int>>, visited: BooleanArray): Int {
+    visited[node] = true
+    var cnt = 0
+
+    for (i in network[node]) {
+        if (!visited[i]) {
+            cnt++
+            cnt += dfs(i, network, visited)
+        }
+    }
+    return cnt
+}
 
 fun main() {
     val computer = readln().toInt()
-    val linkCom = readln().toInt()
-    val graph = Array(computer + 1) { mutableListOf<Int>() }
-
-    repeat(linkCom) {
-        val com = readln().split(" ").map { it.toInt() }
-        graph[com[0]].add(com[1])
-        graph[com[1]].add(com[0])
-    }
-    println(bfs(computer, graph))
-}
-
-fun bfs(computer: Int, graph: Array<MutableList<Int>>): Int {
+    val linkedCom = readln().toInt()
+    val network = List(computer + 1) { mutableListOf<Int>() }
     val visited = BooleanArray(computer + 1)
-    val queue: Queue<Int> = LinkedList()
-    var ans = 0
 
-    queue.offer(1)
-    visited[1] = true
-
-    while (queue.isNotEmpty()) {
-        val node = queue.poll()
-
-        for (neighbor in graph[node]) {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true
-                queue.add(neighbor)
-                ans++
-            }
-        }
+    repeat(linkedCom) {
+        val com = readln().split(" ").map { it.toInt() }
+        network[com[0]].add(com[1])
+        network[com[1]].add(com[0])
     }
-    return ans
+
+    println(dfs(1, network, visited))
 }
