c9f267c050a6a833b7b88885aac2aae53acd4863
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-01-12 14:51:49 +0900

    [Bronze I] Title: 이항 계수 1, Time: 116 ms, Memory: 21244 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.kt
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.kt
@@ -1,13 +1,7 @@
 fun main() {
     val (n, k) = readln().split(" ").map { it.toInt() }
-    println(combination(n, k))
-}
-
-fun combination(n: Int, k: Int): Long {
     var result: Long = 1
-    for (i in 0..<k) {
-        result = result * (n - i) / (i + 1)
-    }
-    return result
-
+    repeat(k) { result = result * (n - it) / (it + 1) }
+    println(result)
 }
+
