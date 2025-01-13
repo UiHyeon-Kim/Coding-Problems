d5c03a4c76e1e5b61181f62ad8a11ce62d41c673
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-01-13 23:52:29 +0900

    [Silver V] Title: 숫자 카드, Time: 1168 ms, Memory: 211132 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.kt
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.kt
@@ -1,28 +1,10 @@
-fun binarySearch(cards: List<Int>, target: Int): Int {
-    var low = 0
-    var high = cards.size - 1
-
-    while (low <= high) {
-        val mid = (low + high) / 2
-        when {
-            cards[mid] == target -> return 1
-            cards[mid] < target -> low = mid + 1
-            else -> high = mid - 1
-        }
-    }
-    return 0
-}
-
 fun main() {
     val n = readln()
-    val cards = readln().split(" ").map { it.toInt() }.sorted()
+    val cards = readln().split(" ").map { it.toInt() }.toSet()
     val m = readln()
     val search = readln().split(" ").map { it.toInt() }
-    val result = mutableListOf<Int>()
 
-    for (target in search) {
-        result.add(binarySearch(cards, target))
-    }
+    val result = search.map { if (it in cards) 1 else 0 }
 
     println(result.joinToString(" "))
 }
