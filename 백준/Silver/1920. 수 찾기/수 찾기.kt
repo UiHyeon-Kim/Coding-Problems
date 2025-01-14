4bb586a0cd99173b228d6ccb623d2b02899a2db8
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-01-14 20:04:23 +0900

    [Silver IV] Title: 수 찾기, Time: 716 ms, Memory: 80848 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val n = readln()
+    val cards = readln().split(" ").map { it.toInt() }.toSet()
+    val m = readln()
+    val search = readln().split(" ").map { it.toInt() }
+
+    val result = search.map { if (it in cards) 1 else 0 }
+
+    println(result.joinToString("\n"))
+}
