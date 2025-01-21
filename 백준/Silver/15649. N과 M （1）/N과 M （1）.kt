e2e0986f2bc7ab0e96eddb8c088d64da24c7f71c
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-01-21 11:12:55 +0900

    [Silver III] Title: N과 M (1), Time: 216 ms, Memory: 25332 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -1,24 +1,36 @@
-fun main() {
-    val (n, m) = readln().split(" ").map { it.toInt() }
-    val visited = BooleanArray(n + 1)
-    val current = mutableListOf<Int>()
+import java.lang.StringBuilder
+import java.util.*
 
-    fun backTrack(depth: Int) {
-        if (depth == m) {
-            println(current.joinToString(" "))
+fun main() = with(Scanner(System.`in`)){
+    val n = nextInt()
+    val m = nextInt()
+    val visit = Array(n+1){false} // 방문 여부
+    val arr = Array(m){0} // 수열 결과
+    val sb = StringBuilder()
+
+    fun find(len : Int){
+        if(len == m) {
+            // 문자열 만들기
+            arr.forEach {
+                sb.append(it).append(' ')
+            }
+            sb.append('\n')
             return
         }
 
-        for (i in 1..n) {
-            if (!visited[i]) {
-                visited[i] = true
-                current.add(i)
-                backTrack(depth + 1)
-                visited[i] = false
-                current.removeAt(current.lastIndex)
+        for(i in 1 .. n){
+            // 포함되지 않았다면
+            if(!visit[i]){
+                // 방문 처리
+                visit[i] = true
+                arr[len] = i
+                find(len+1)
+                // 다른 탐색을 위해 false 처리
+                visit[i] = false
             }
         }
     }
 
-    backTrack(0)
+    find(0)
+    println(sb) // 한번에 출력
 }
