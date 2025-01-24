aa4a77f6db03f3fe2047b9662229a5c5eb2a74cb
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-01-24 18:12:08 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 21312 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
@@ -3,16 +3,16 @@ fun main() {
     val current = mutableListOf<Int>()
     val sb = StringBuilder()
 
-    fun backTrack(start: Int) {
+    fun backTrack(num: Int) {
         if (current.size == m) {
             sb.append(current.joinToString(" ")).append("\n")
             return
         }
 
-        for (i in start..n) {
+        for (i in num..n) {
             current.add(i)
             backTrack(i + 1)
-            current.removeAt(current.size - 1)
+            current.removeAt(current.lastIndex)
         }
     }
 
