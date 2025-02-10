31cc09fa1d8b2fae4c31cddc6965aefede9d5f27
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-02-10 15:43:30 +0900

    [level 2] Title: 가장 큰 수, Time: 210.47 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(numbers: IntArray): String {
+        var answer =  numbers.map { it.toString() }
+            .sortedWith { a, b -> (b + a).compareTo(a + b) }
+            .joinToString("")
+        
+        return if(answer[0] == '0') "0" else answer
+    }
+}
