31cf6d7b99370707a19c5039af0d742c1a48762e
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-02-10 12:26:42 +0900

    [level 1] Title: K번째수, Time: 32.30 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.kt
+++ 프로그래머스/1/42748. K번째수/K번째수.kt
@@ -1,12 +1,7 @@
 class Solution {
-    fun solution(array: IntArray, commands: Array<IntArray>): List<Int> {
-        var answer = mutableListOf<Int>()
-        
-        commands.forEach { command ->
-            val sub = array.slice(command[0]-1 until command[1]).sorted()
-            answer.add(sub[command[2] - 1])
-        }
-        
-        return answer
+    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+        return commands.map { command ->
+            array.slice(command[0] - 1 until command[1]).sorted()[command[2] - 1]
+        }.toIntArray()
     }
 }
