92d6b94b416d08d4298af5ff6cb60e33d44c3b6b
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-02-21 15:01:25 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 16932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.kt
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.kt
@@ -3,14 +3,11 @@ fun main() {
     val dp = IntArray(n + 1) { Int.MAX_VALUE }
 
     dp[1] = 0
-    if (n >= 2) dp[2] = 1
-    if (n >= 3) dp[3] = 1
 
-    for (i in 4..n) {
-        if (i % 2 == 0 && i % 3 == 0) dp[i] = minOf(dp[i - 1], dp[i / 2], dp[i / 3]) + 1
-        else if (i % 2 == 0) dp[i] = minOf(dp[i - 1], dp[i / 2]) + 1
-        else if (i % 3 == 0) dp[i] = minOf(dp[i - 1], dp[i / 3]) + 1
-        else dp[i] = dp[i - 1] + 1
+    for (i in 2..n) {
+        dp[i] = dp[i - 1] + 1
+        if (i % 3 == 0) dp[i] = minOf(dp[i], dp[i / 3] + 1)
+        if (i % 2 == 0) dp[i] = minOf(dp[i], dp[i / 2] + 1)
     }
 
     println(dp[n])
