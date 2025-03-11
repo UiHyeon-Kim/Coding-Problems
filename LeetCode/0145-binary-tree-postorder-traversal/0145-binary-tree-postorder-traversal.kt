3ba9e304e6cd230b3b74b01e0e960d615e3a21d0
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-03-12 01:22:06 +0900

    Time: 0 ms (100%), Space: 41.4 MB (5.75%) - LeetHub

--- LeetCode/0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.kt
+++ LeetCode/0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.kt
@@ -1,8 +1,4 @@
 /**
- * Example:
- * var ti = TreeNode(5)
- * var v = ti.`val`
- * Definition for a binary tree node.
  * class TreeNode(var `val`: Int) {
  *     var left: TreeNode? = null
  *     var right: TreeNode? = null
