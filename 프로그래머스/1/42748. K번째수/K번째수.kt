fe43e610bad2ebd857f7b29e54b416a2804e372f
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-03-16 11:48:26 +0900

    [level 1] Title: K번째수, Time: 33.22 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.kt
+++ 프로그래머스/1/42748. K번째수/K번째수.kt
@@ -1,7 +1,5 @@
 class Solution {
-    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
-        return commands.map { command ->
-            array.slice(command[0] - 1 until command[1]).sorted()[command[2] - 1]
-        }.toIntArray()
-    }
+    fun solution(array: IntArray, commands: Array<IntArray>) = commands.map {
+            array.slice((it[0] - 1) until it[1]).sorted()[it[2] - 1]
+        }
 }
