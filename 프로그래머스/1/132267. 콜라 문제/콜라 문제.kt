cd2e9ada083f74c88df8e8ca30a0b9920f22edd1
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-03-17 14:41:52 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
@@ -1,11 +1,5 @@
 class Solution {
-    fun solution(a: Int, b: Int, n: Int) = getEmptyBottle(a, b, n, 0)
-    
-    fun getEmptyBottle(a: Int, b: Int, n: Int, total: Int) : Int {
-        if (n < a) return total
-        
-        var currentBottle = n % a + (n / a * b)
-        
-        return getEmptyBottle(a, b, currentBottle, total + n / a * b)
+    fun solution(a: Int, b: Int, n: Int): Int {
+        return (if (n > b) n - b else 0) / (a - b) * b
     }
 }
