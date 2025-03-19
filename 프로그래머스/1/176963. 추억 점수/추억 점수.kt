ab9ee001579e611b30567cb7b10432dc46a49a16
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-03-19 10:23:01 +0900

    [level 1] Title: 추억 점수, Time: 27.01 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.kt
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.kt
@@ -1,9 +1,14 @@
 class Solution {
     fun solution(names: Array<String>, yearning: IntArray, photos: Array<Array<String>>): IntArray {
+        var answer = IntArray(photos.size)
         val memory = names.zip(yearning.toList()).toMap()
-    
-        return photos.map { photo ->
-            photo.sumOf { memory[it] ?: 0 }
-        }.toIntArray()
+        
+        photos.forEachIndexed { index, photo ->
+            photo.forEach { person ->
+                answer[index] += memory.getOrDefault(person, 0)
+            }
+        }
+        
+        return answer
     }
 }
