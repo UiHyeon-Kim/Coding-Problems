f045132406bcf50202015f0e562f94b3bc540f8e
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-10-02 16:11:41 +0900

    [Silver II] Title: 외판원 순회 2, Time: 224 ms, Memory: 22140 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val n = readln().toInt()
+    val routes = Array(n) { readln().split(" ").map { it.toInt() } }
+    val visited = BooleanArray(n).apply { this[0] = true }
+    var result = Int.MAX_VALUE
+
+    fun backtrack(cost: Int, city: Int) {
+        if (visited.all { it }) {
+            if (routes[city][0] != 0) {
+                result = minOf(result, cost + routes[city][0])
+            }
+            return
+        }
+
+        for (i in 0 until n) {
+            if (visited[i] || routes[city][i] == 0) continue
+            visited[i] = true
+            backtrack(cost + routes[city][i], i)
+            visited[i] = false
+        }
+    }
+    backtrack(0, 0)
+    println(result)
+}
