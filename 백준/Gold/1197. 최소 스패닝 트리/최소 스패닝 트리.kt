763a0fb802e1d0b17d19b2bfcd7d4a0c759da81d
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-11-12 16:52:59 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 984 ms, Memory: 179292 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+data class Edge(val to: Int, val weight: Int)
+
+fun main() {
+    val (v, e) = readln().split(" ").map { it.toInt() }
+    val graph = Array(v + 1) { mutableListOf<Edge>() }
+
+    repeat(e) {
+        val (start, end, weight) = readln().split(" ").map { it.toInt() }
+        graph[start].add(Edge(end, weight))
+        graph[end].add(Edge(start, weight))
+    }
+
+    val pq = PriorityQueue(compareBy<Edge> { it.weight })
+    val visited = BooleanArray(v + 1)
+    var total = 0
+
+    pq.add(Edge(1, 0))
+
+    while (pq.isNotEmpty()) {
+        val (node, weight) = pq.poll()
+        if (visited[node]) continue
+        visited[node] = true
+        total += weight
+
+        for (neighbor in graph[node]) {
+            if (!visited[neighbor.to]) pq.add(Edge(neighbor.to, neighbor.weight))
+        }
+    }
+
+    println(total)
+}
