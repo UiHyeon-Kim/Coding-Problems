9a639183979a41e06bf9a4782c19efbf727d40b3
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2026-01-08 18:40:37 +0900

    [Silver I] Title: 부동산 다툼, Time: 404 ms, Memory: 48492 KB -BaekjoonHub

--- 백준/Silver/20364. 부동산 다툼/부동산 다툼.kt
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.kt
@@ -1,22 +1,48 @@
+class Baekjoon20364 {
+
+    private lateinit var purchased: BooleanArray
+    private lateinit var sb: StringBuilder
+
+    fun solve() = with(System.`in`.bufferedReader()) {
+
+        val (totalLand, ducks) = readLine().split(" ").map(String::toInt)
+
+        purchased = BooleanArray(totalLand + 1)
+        sb = StringBuilder()
+
+        repeat(ducks) {
+            val wantLand = readLine().toInt()
+
+            findAvailableLand(wantLand)
+        }
+
+        println(sb)
+    }
+
+    /** 자식 -> 부모 방향 탐색 */
+    private fun findAvailableLand(wantLand: Int) {
+        var currentLand = wantLand  // 현재 위치하는 땅
+        var nonePass = 0            // 막히는 땅
+
+        while (currentLand > 0) {
+
+            // 현재 위치한 땅이 이미 구매된 땅이면 막힌 땅 체크
+            if (purchased[currentLand]) {
+                nonePass = currentLand
+            }
+
+            currentLand /= 2
+        }
+
+        // 막는 땅이 없으면 방문처리
+        if (nonePass == 0) {
+            purchased[wantLand] = true
+        }
+
+        sb.append(nonePass).append('\n')
+    }
+}
+
 fun main() {
-    val (n, q) = readln().split(" ").map { it.toInt() }
-    val visited = mutableSetOf<Int>()
-    var min = Int.MAX_VALUE
-
-    fun dfs(node: Int) {
-        if (node in visited) min = node
-        if (node == 1) return
-
-        if (node % 2 == 0) dfs(node / 2)
-        else dfs((node - 1) / 2)
-    }
-
-    repeat(q) {
-        val target = readln().toInt()
-        min = Int.MAX_VALUE
-
-        dfs(target)
-        visited.add(target)
-        println(if (min == Int.MAX_VALUE) 0 else min)
-    }
+    Baekjoon20364().solve()
 }
