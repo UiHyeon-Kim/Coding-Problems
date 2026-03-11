24363f41590f3fddeffb59509deec1f236833e21
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2026-03-11 21:46:38 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 84 ms, Memory: 12624 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.kt
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.kt
@@ -1,16 +1,24 @@
-fun main() = with(System.`in`.bufferedReader()){
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
     val case = readLine().toInt()
+    val sb = StringBuilder()
 
     repeat(case) {
-        val clotheCount = readLine().toInt()
-        val clothes = mutableMapOf<String, Int>()
+        val clothesCount =  readLine().toInt()
+        val clothes = HashMap<String, Int>()
 
-        repeat(clotheCount) {
-            val (_, type) = readLine().split(' ')
+        repeat(clothesCount) {
+            val st = StringTokenizer(readLine())
+            st.nextToken()
+            val type = st.nextToken()
+
             clothes[type] = clothes.getOrDefault(type, 1) + 1
         }
 
         val result = clothes.values.fold(1) { acc, value -> acc * value }
-        println(result - 1)
+        sb.append(result - 1).append("\n")
     }
+
+    println(sb)
 }
