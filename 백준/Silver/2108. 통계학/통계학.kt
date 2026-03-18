d563bbad01c4e7ec8334a6c1090b20c36bda394f
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2026-03-18 21:37:48 +0900

    [Silver II] Title: 통계학, Time: 452 ms, Memory: 51228 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.kt
+++ 백준/Silver/2108. 통계학/통계학.kt
@@ -15,15 +15,22 @@ fun main() = with(System.`in`.bufferedReader()) {
     sb.append("%.0f".format(sum.toDouble() / n).toInt()).append("\n")
     sb.append(arr[n / 2]).append("\n")
 
-    val frequencyMap = mutableMapOf<Int, Int>()
-    for (num in arr) {
-        frequencyMap[num] = frequencyMap.getOrDefault(num, 0) + 1
+    var maxFrequency = 0
+    for (num in count) {
+        if (num > maxFrequency) maxFrequency = num
     }
 
-    val maxValue = frequencyMap.maxBy { it.value }.value
-    val key = frequencyMap.filter { maxValue == it.value }.keys.sorted()
+    var mode = 0
+    var second = false
+    for (i in 0..8000) {
+        if (count[i] == maxFrequency) {
+            mode = i - 4000
+            if (second) break
+            second = true
+        }
+    }
 
-    sb.append(if (key.size > 1) key[1] else key[0]).append("\n")
+    sb.append(mode).append("\n")
     sb.append(arr.last() - arr.first())
 
     println(sb)
