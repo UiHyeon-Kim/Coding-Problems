aeb465ae00c53d428a83488c51879bd0ca8af63a
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2026-03-25 21:13:34 +0900

    [Gold IV] Title: 연구소, Time: 604 ms, Memory: 167760 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.kt
+++ 백준/Gold/14502. 연구소/연구소.kt
@@ -1,58 +1,83 @@
-fun main() {
-    val (rowSize, colSize) = readln().split(" ").map { it.toInt() }
-    val emptyIndex = mutableListOf<Pair<Int, Int>>() // 벽을 세울 수 있는 빈 공간 인덱스
-    val board = Array(rowSize) { row ->
-        readln().split(" ").map { it.toInt() }.toIntArray().also {
-            it.forEachIndexed { col, value -> if (value == 0) emptyIndex.add(row to col) }
-        }
-    }
+package `14502 연구소`
 
-    val direction = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
-    var max = 0
+import java.util.StringTokenizer
 
-    // 보드 감염 및 감염 후 0개수 반환
-    fun infection(): Int {
-        val currentBoard = Array(rowSize) { board[it].clone() }
-        val q = ArrayDeque<Pair<Int, Int>>()
+class Baekjoon14502() {
 
-        for (i in 0 until rowSize) {
-            for (j in 0 until colSize) {
-                if (currentBoard[i][j] == 2) q.add(i to j)
+    private lateinit var lab: Array<IntArray>
+    private lateinit var infectiousAgents: MutableList<Pair<Int, Int>>
+    private lateinit var emptyArea: MutableList<Pair<Int, Int>>
+    private var r = 0
+    private var c = 0
+    private var max = 0
+
+    fun solve() = with(System.`in`.bufferedReader()) {
+        var st = StringTokenizer(readLine())
+        r = st.nextToken().toInt()
+        c = st.nextToken().toInt()
+
+        infectiousAgents = mutableListOf()
+        emptyArea = mutableListOf()
+
+        lab = Array(r) { i ->
+            st = StringTokenizer(readLine())
+
+            IntArray(c) { j ->
+                val current = st.nextToken().toInt()
+
+                if (current == 2) infectiousAgents.add(i to j)
+                else if (current == 0) emptyArea.add(i to j)
+
+                current
             }
         }
 
-        while (q.isNotEmpty()) {
-            val (cr, cc) = q.removeFirst()
+        buildWall(0, 0)
+        println(max)
+    }
+
+    private fun buildWall(startIdx: Int, count: Int) {
+        if (count == 3) {
+            max = max.coerceAtLeast(getSafeArea(inspection()))
+            return
+        }
+
+        for (i in startIdx..<emptyArea.size) {
+            val (wr, wc) = emptyArea[i]
+            lab[wr][wc] = 1
+            buildWall(i + 1, count + 1)
+            lab[wr][wc] = 0
+        }
+    }
+
+    private fun getSafeArea(currentLab: Array<IntArray>): Int =
+        currentLab.sumOf { it.count { area -> area == 0 } }
+
+    private fun inspection(): Array<IntArray>{
+        val direction = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
+        val copyLab = Array(r) { lab[it].clone() }
+
+        val dq = ArrayDeque<Pair<Int, Int>>()
+        dq.addAll(infectiousAgents)
+
+        while (dq.isNotEmpty()) {
+            val (cr, cc) = dq.removeFirst()
 
             for ((dr, dc) in direction) {
                 val nr = cr + dr
                 val nc = cc + dc
 
-                if (nr !in 0 until rowSize || nc !in 0 until colSize) continue
-                if (currentBoard[nr][nc] == 0) {
-                    currentBoard[nr][nc] = 2
-                    q.add(nr to nc)
+                if (nr in 0..<r && nc in 0..<c && copyLab[nr][nc] == 0) {
+                    copyLab[nr][nc] = 2
+                    dq.add(nr to nc)
                 }
             }
         }
 
-        return currentBoard.sumOf { it.count { value -> value == 0 } }
+        return copyLab
     }
+}
 
-    fun backtrack(start: Int, wall: Int) {
-        if (wall == 3) {
-            max = max.coerceAtLeast(infection())
-            return
-        }
-
-        for (i in start until emptyIndex.size) {
-            val (r, c) = emptyIndex[i]
-            board[r][c] = 1
-            backtrack(i + 1, wall + 1)
-            board[r][c] = 0
-        }
-    }
-
-    backtrack(0, 0)
-    println(max)
+fun main() {
+    Baekjoon14502().solve()
 }
