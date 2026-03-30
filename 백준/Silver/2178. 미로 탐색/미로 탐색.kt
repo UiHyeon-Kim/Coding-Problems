42290e6bb747d925f44739aab6d5bfb774ccd4cf
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2026-03-30 16:34:46 +0900

    [Silver I] Title: 미로 탐색, Time: 164 ms, Memory: 24076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.kt
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.kt
@@ -1,30 +1,55 @@
-fun main() {
-    val (row, col) = readln().split(" ").map { it.toInt() }
-    val miro = Array(row) { readln().map { it.digitToInt() } }
-    val miroLen = Array(row) { IntArray(col) { -1 } }
-    val direction = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
+import java.util.StringTokenizer
 
-    fun bfs(start: Pair<Int, Int>) {
+class Baekjoon2178 {
+
+    private lateinit var miro: Array<IntArray>
+    private var r = 0
+    private var c = 0
+
+    fun solve() = with(System.`in`.bufferedReader()) {
+        val st = StringTokenizer(readLine())
+        r = st.nextToken().toInt()
+        c = st.nextToken().toInt()
+
+        miro = Array(r) {
+            val row = readLine()
+
+            IntArray(c) { i ->
+                row[i].digitToInt()
+            }
+        }
+
+        println(bfs())
+    }
+
+    private fun bfs(): Int {
+        val dr = listOf(-1, 1, 0, 0)
+        val dc = listOf(0, 0, -1, 1)
+
         val q = ArrayDeque<Pair<Int, Int>>()
-        q.add(start)
-        miroLen[start.first][start.second] = 1
+        val visited = Array(r) { IntArray(c) { -1 } }
+
+        q.add(0 to 0)
+        visited[0][0] = 1
 
         while (q.isNotEmpty()) {
-            val (cx, cy) = q.removeFirst()
+            val (cr, cc) = q.removeFirst()
 
-            for ((dx, dy) in direction) {
-                val nx = cx + dx
-                val ny = cy + dy
+            for (i in 0..<4) {
+                val nr = cr + dr[i]
+                val nc = cc + dc[i]
 
-                if (nx in 0 until row && ny in 0 until col && miro[nx][ny] == 1 && miroLen[nx][ny] == -1) {
-                    miroLen[nx][ny] = miroLen[cx][cy] + 1
-                    q.add(nx to ny)
+                if (nr in 0..<r && nc in 0..<c && miro[nr][nc] == 1 && visited[nr][nc] == -1) {
+                    q.add(nr to nc)
+                    visited[nr][nc] = visited[cr][cc] + 1
                 }
             }
         }
+
+        return visited[r - 1][c - 1]
     }
+}
 
-    bfs(0 to 0)
-
-    println(miroLen[row - 1][col - 1])
+fun main() {
+    Baekjoon2178().solve()
 }
