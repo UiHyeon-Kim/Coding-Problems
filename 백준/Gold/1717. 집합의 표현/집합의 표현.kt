343a19feb8f580d02b4f43d5a8e33ec6f666543e
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2026-04-07 16:42:55 +0900

    [Gold V] Title: 집합의 표현, Time: 764 ms, Memory: 60128 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.kt
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.kt
@@ -1,62 +1,34 @@
 import java.util.StringTokenizer
 
-class Baekjoon1717 {
+fun main() = with(System.`in`.bufferedReader()) {
+    var st = StringTokenizer(readLine())
+    val n = st.nextToken().toInt()
+    val m = st.nextToken().toInt()
+    val parent = IntArray(n + 1) { it }
+    val sb = StringBuilder()
 
-    private lateinit var parent: IntArray
-    private lateinit var rank: IntArray
-
-    fun solve() = with(System.`in`.bufferedReader()) {
-        var st = StringTokenizer(readLine())
-        val n = st.nextToken().toInt()
-        val m = st.nextToken().toInt()
-        val sb = StringBuilder()
-
-        parent = IntArray(n + 1) { it }
-        rank = IntArray(n + 1)
-
-        repeat(m) {
-            st = StringTokenizer(readLine())
-            val cmd = st.nextToken().toInt()
-            val a = st.nextToken().toInt()
-            val b = st.nextToken().toInt()
-
-            if (cmd == 0) {
-                union(a, b)
-            } else {
-                if (find(a) == find(b)) {
-                    sb.append("YES").append("\n")
-                } else {
-                    sb.append("NO").append("\n")
-                }
-            }
-        }
-
-        println(sb)
-    }
-
-    private fun find(x: Int): Int {
+    fun find(x: Int): Int {
         if (parent[x] == x) return x
         parent[x] = find(parent[x])
         return parent[x]
     }
 
-    private fun union(a: Int, b: Int) {
-        val rootA = find(a)
-        val rootB = find(b)
+    repeat(m) {
+        st = StringTokenizer(readLine())
+        val cmd = st.nextToken().toInt()
+        val a = st.nextToken().toInt()
+        val b = st.nextToken().toInt()
 
-        if (rootA != rootB) {
-            if (rank[rootA] < rank[rootB]) {
-                parent[rootA] = rootB
-            } else if (rank[rootB] < rank[rootA]) {
-                parent[rootB] = rootA
-            } else {
-                parent[rootB] = rootA
-                rank[rootA]++
-            }
+        if (cmd == 0) {
+            val ap = find(a)
+            val bp = find(b)
+            if (ap != bp) parent[ap] = bp
+        } else {
+            sb.append(
+                if (find(a) == find(b)) "YES" else "NO"
+            ).append("\n")
         }
     }
-}
 
-fun main() {
-    Baekjoon1717().solve()
+    println(sb)
 }
