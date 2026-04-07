d327051f338003ce2fbd468b1c82f9b1762526f2
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2026-04-07 18:19:59 +0900

    [Gold IV] Title: 최단경로, Time: 776 ms, Memory: 138948 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.kt
+++ 백준/Gold/1753. 최단경로/최단경로.kt
@@ -1,48 +1,63 @@
+package `1753 최단경로`
+
 import java.util.PriorityQueue
 import java.util.StringTokenizer
 
-data class Node(val to: Int, val weight: Int)
+class Baekjoon1753 {
 
-fun main() = with(System.`in`.bufferedReader()) {
-    var st = StringTokenizer(readLine())
-    val V = st.nextToken().toInt()  // 정점 수
-    val E = st.nextToken().toInt()  // 간선 수
-    val K = readLine().toInt()      // 시작 정점 번호
-    val graph = Array(V + 1) { ArrayList<Node>() }  // 그래프 정보
+    data class Node(val to: Int, val weight: Int)
 
-    repeat(E) {
-        st = StringTokenizer(readLine())
-        val u = st.nextToken().toInt()
-        val v = st.nextToken().toInt()
-        val w = st.nextToken().toInt()
+    private lateinit var graph: Array<ArrayList<Node>>
+    private lateinit var dist: IntArray
 
-        graph[u].add(Node(v, w))
+    fun solve() = with(System.`in`.bufferedReader()) {
+        var st = StringTokenizer(readLine())
+        val V = st.nextToken().toInt()
+        val E = st.nextToken().toInt()
+        val K = readLine().toInt()
+
+        graph = Array(V + 1) { ArrayList() }
+        dist = IntArray(V + 1) { Int.MAX_VALUE }
+        val sb = StringBuilder()
+
+        repeat(E) {
+            st = StringTokenizer(readLine())
+            val u = st.nextToken().toInt()
+            val v = st.nextToken().toInt()
+            val w = st.nextToken().toInt()
+
+            graph[u].add(Node(v, w))
+        }
+        bfs(K)
+
+        for (i in 1..V) {
+            sb.append(
+                if (dist[i] == Int.MAX_VALUE) "INF" else dist[i]
+            ).append("\n")
+        }
+
+        println(sb)
     }
 
-    val pq = PriorityQueue<Node>(compareBy { it.weight })
-    val dist = IntArray(V + 1) { Int.MAX_VALUE } // 최단거리 정보
+    private fun bfs(k: Int) {
 
-    pq.add(Node(K, 0))
-    dist[K] = 0
+        val pq = PriorityQueue<Node>(compareBy { it.weight })
 
-    while (pq.isNotEmpty()) {
-        val (curStart, curDist) = pq.poll()
+        pq.add(Node(k, 0))
+        dist[k] = 0
 
-        if (curDist > dist[curStart]) continue
-
-        for (node in graph[curStart]) {
-            val to = node.to
-            val weight = node.weight
-
-            if (dist[to] > weight + curDist) {
-                dist[to] = weight + curDist
-                pq.add(Node(to, weight + curDist))
+        while (pq.isNotEmpty()) {
+            val current = pq.poll()
+            for (neighbor in graph[current.to]) {
+                if (dist[neighbor.to] > current.weight + neighbor.weight) {
+                    dist[neighbor.to] = current.weight + neighbor.weight
+                    pq.add(Node(neighbor.to, current.weight + neighbor.weight))
+                }
             }
         }
     }
+}
 
-    for (i in 1..V) {
-        if (dist[i] == Int.MAX_VALUE) println("INF")
-        else println(dist[i])
-    }
+fun main() {
+    Baekjoon1753().solve()
 }
