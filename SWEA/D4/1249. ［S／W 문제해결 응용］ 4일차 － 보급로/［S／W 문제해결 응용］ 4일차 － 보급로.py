befc669ae6959d9993f5eb3ceb186caacdb2d57d
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2026-05-15 15:23:46 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 199 ms, Memory: 64,768 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,38 +1,32 @@
 import heapq
 
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
 
-def solve():
-    T = int(input())
+T = int(input())
 
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
-
-    for tc in range(1, T + 1):
-        board_size = int(input())
-        board = [list(map(int, input())) for _ in range(board_size)]
-
-        pq = [(0, 0, 0)]  # w, r, c
-        dist = [[float('inf')] * board_size for _ in range(board_size)]
-        dist[0][0] = 0
-
-        while pq:
-            w, r, c = heapq.heappop(pq)
-
-            if dist[r][c] < w:
-                continue
-
-            for i in range(4):
-                nr = r + dr[i]
-                nc = c + dc[i]
-
-                if 0 <= nr < board_size and 0 <= nc < board_size:
-                    weight = w + board[nr][nc]
-                    if dist[nr][nc] > weight:
-                        heapq.heappush(pq, (weight, nr, nc))
-                        dist[nr][nc] = weight
-
-        print(f"#{tc} {dist[-1][-1]}")
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(map(int, input())) for _ in range(N)]
+    dist = [[float('inf')] * N for _ in range(N)]
+    dist[0][0] = 0
+    pq = [(0, 0, 0)]
 
 
-if __name__ == "__main__":
-    solve()
+    while pq:
+        w, r, c = heapq.heappop(pq)
+
+        if w > dist[r][c]:
+            continue
+
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if 0 <= nr < N and 0 <= nc < N:
+                cost = w + arr[nr][nc]
+                if cost < dist[nr][nc]:
+                    dist[nr][nc] = cost
+                    heapq.heappush(pq, (cost, nr, nc))
+
+    print(f"#{tc} {dist[N - 1][N - 1]}")
